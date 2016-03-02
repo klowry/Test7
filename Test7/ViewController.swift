213cd03c483677eb454653f49eaf485aeec57e68
Kelly <lowrykr@gmail.com>
2016-03-02 16:52:12 -0600

    Test7

---
+++ Test7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test7
+//
+//  Created by Kelly-MacBook on 3/2/16.
+//  Copyright © 2016 Kelly-MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
